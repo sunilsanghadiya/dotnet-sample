5edcac86ce858f3db62cb34e5c704383910d77a5
Hardik Joshi <hardik.joshi@techabbot.com>
2025-06-04 15:05:40 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_sample.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+        }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to dotnet sample API");
+        }
+    }
+}
