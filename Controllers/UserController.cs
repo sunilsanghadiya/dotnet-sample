ea704d8eab6e112408e4ac3bc7c0d1d65f4d30d5
Hardik Joshi <hardik.joshi@techabbot.com>
2025-06-04 15:17:12 +0530

    added new controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_sample.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController() { }
+
+        [HttpGet("GetUser")]
+        public IActionResult GetUser()
+        {
+            return Ok("Successfully get user");
+        }
+    }
+}
